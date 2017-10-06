b4e7c309be59ceefb9778efa345ae2d7fa43bd23
Mikki <brin5845@gmail.com>
2017-10-06 17:08:10 -0600

    notes about GitHub/git

--- Rstudio-intro/hello-world.R
+++ Rstudio-intro/hello-world.R
@@ -1,1 +1,7 @@
 print("hello world, R is super hard. Don't worry, it gets easier!")
+# git branch mikki # mikki is the name of the branch
+# from command line
+# git checkout branchname
+
+# push a local branch to the master branch (origin)
+# git push -u origin mikki
