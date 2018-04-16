0a6274b4d31ed3ef2ca0cf91ce25a8143012e57d
Kyle Shannon <kyle@pobox.com>
2018-04-16 12:23:14 -0600

    add copyright

--- web/web.R
+++ web/web.R
@@ -1,3 +1,7 @@
+# Copyright (c) 2018, Boise State University All rights reserved.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
+
 library("RCurl")
 library("rjson")
 
@@ -47,3 +51,4 @@ for(p in x$properties$periods) {
 }
 
 # DECODE OMIT
+
